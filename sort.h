37ece5f715c0898175bbc857a37429f07cb8943a
veronica1013 <veronica.isiaho@gmail.com>
2024-10-29 14:03:03 -0700

    Adding files

--- sort.h
+++ sort.h
@@ -1,9 +1,28 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-/* Doubly linked list node structure */
+/*Comparison direction macros for bitonic sort*/
+#define UP 0
+#define DOWN 1
+
+/*enum bool - Enumeration of boolean values*/
+typedef enum bool
+{
+    false = 0,
+    true
+} bool;
+
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
 typedef struct listint_s
 {
     const int n;
@@ -11,9 +30,17 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-/* Function prototypes */
+/*Helper for swapping*/
+void swap_ints(int *a, int *b);
+
+/*Printing helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void bubble_sort(int *array, size_t size);  /* Example sorting function prototype */
+
+/*Regular functions prototypes*/
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */
